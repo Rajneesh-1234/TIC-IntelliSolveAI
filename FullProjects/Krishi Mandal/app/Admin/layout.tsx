39c99979b61725caca27f5bf44fb43da150c5ac6
Golu <golusiddharth88@gmail.com>
2026-04-04 21:54:03 +0530

    remove

--- FullProjects/Krishi Mandal/app/Admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardLayout } from "@/components/dashboard-layout"
-
-export default function GovernmentLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>
-}
